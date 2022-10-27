560286f77c68e6a12f84688d73633e94ada69269
Ezequiel Russo <ezequiel.rube@gmail.com>
2022-10-27 11:45:51 +0200

    Generated new type of corrupted data

--- go.mod
+++ go.mod
@@ -3,15 +3,19 @@ module github.com/erusso7/fraud-sample-generator
 go 1.18
 
 require (
+	github.com/urfave/cli/v2 v2.20.3
 	github.com/xitongsys/parquet-go v1.6.2
-	github.com/xitongsys/parquet-go-source v0.0.0-20220723234337-052319f3f36b
+	github.com/xitongsys/parquet-go-source v0.0.0-20221025031416-9877e685ef65
 )
 
 require (
 	github.com/apache/arrow/go/arrow v0.0.0-20200730104253-651201b0f516 // indirect
 	github.com/apache/thrift v0.14.2 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/golang/snappy v0.0.3 // indirect
-	github.com/klauspost/compress v1.13.1 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/pierrec/lz4/v4 v4.1.8 // indirect
-	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- main.go
+++ main.go
@@ -5,123 +5,83 @@ import (
 	"github.com/xitongsys/parquet-go-source/local"
 	"github.com/xitongsys/parquet-go/writer"
 	"log"
-	"math"
 	"math/rand"
 	"os"
-	"strconv"
-	"strings"
 	"time"
-)
 
-type TransType string
-
-const (
-	TransTypeChip        TransType = "chip_and_pin"
-	TransTypeContactLess TransType = "contactless"
-	TransTypeOnline      TransType = "online"
-	TransTypeSwipe       TransType = "swipe"
-	TransTypeManual      TransType = "manual"
-)
-
-type Label string
-
-const (
-	LabelLegitimate Label = "legitimate"
-	LabelFraud            = "fraud"
+	"github.com/urfave/cli/v2"
 )
 
 const (
-	sep           = "\t"
-	initialDate   = 946_684_800 // 2000-01-01 00:00:00
-	maxUserID     = 10_000
-	maxMerchantID = 20_000
-	minMerchantID = 1_000
+	ArgRows = "rows"
+	ArgCp   = "corrupt-percentage"
+	ArgCt   = "corrupt-type"
 )
 
-var offset = 0
-var labels = []Label{LabelLegitimate, LabelFraud}
-var transTypes = []TransType{TransTypeChip, TransTypeContactLess, TransTypeOnline, TransTypeSwipe, TransTypeManual}
-var corrupted = 0.0
+type CorruptType string
 
-func NewRow(errorRatio int) interface{} {
-	offset += rand.Intn(10)
-
-	r := Row{
-		Timestamp:    initialDate + offset,
-		Label:        labels[rand.Intn(len(labels))],
-		UserId:       rand.Intn(maxUserID),
-		Amount:       math.Round(rand.Float64()*float64(rand.Intn(1000)*1000)) / 1000,
-		MerchantId:   rand.Intn(maxMerchantID) + minMerchantID,
-		TransType:    transTypes[rand.Intn(len(transTypes))],
-		Foreign:      rand.Intn(2) == 1,
-		InterArrival: math.Round(rand.Float64()*10_000*1000) / 1000,
-	}
-
-	if rand.Intn(100) < errorRatio {
-		corrupted++
-		return corruptRow(r)
-	}
-
-	return r
-}
-
-func (r Row) String() string {
-	var parts []string
-	rowProps := []string{
-		fmt.Sprintf("%d", r.Timestamp),
-		string(r.Label),
-		fmt.Sprintf("%d", r.UserId),
-		fmt.Sprintf("%.3f", r.Amount),
-		fmt.Sprintf("%d", r.MerchantId),
-		string(r.TransType),
-		fmt.Sprintf("%t", r.Foreign),
-		fmt.Sprintf("%.3f", r.Amount),
-	}
-	for _, s := range rowProps {
-		if strings.TrimSpace(s) != "" {
-			parts = append(parts, s)
-		}
-	}
-	return strings.Join(parts, sep)
-}
+const (
+	CorruptMissing CorruptType = "missing"
+	CorruptEmpty   CorruptType = "empty"
+)
 
 func main() {
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.UintFlag{
+				Name:        ArgRows,
+				Aliases:     []string{"r"},
+				Value:       100,
+				Usage:       "number of rows to generate",
+				DefaultText: "100",
+			},
+			&cli.StringFlag{
+				Name:        ArgCt,
+				Aliases:     []string{"ct"},
+				Value:       "missing",
+				Usage:       "the corruption type [accepts: missing, empty]",
+				DefaultText: "missing",
+			},
+			&cli.UintFlag{
+				Name:        ArgCp,
+				Aliases:     []string{"cp"},
+				Value:       20,
+				Usage:       "the approximate percentage of corrupted data",
+				DefaultText: "20",
+			},
+		},
+		Action: generateRows,
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func generateRows(cCtx *cli.Context) error {
 	rand.Seed(time.Now().UnixNano())
 	var err error
 
-	num := 100
-	if len(os.Args) > 1 {
-		arg, err := strconv.ParseInt(os.Args[1], 10, 32)
-		if err != nil {
-			log.Fatal(err)
-		}
-		num = int(arg)
-	}
+	num := cCtx.Uint(ArgRows)
+	cRatio := cCtx.Uint(ArgCp)
+	cType := CorruptType(cCtx.String(ArgCt))
 
-	errorRatio := 0
-	if len(os.Args) > 2 {
-		arg, err := strconv.ParseInt(os.Args[2], 10, 32)
-		if err != nil {
-			log.Fatal(err)
-		}
-		errorRatio = int(arg)
-	}
+	fileName := fmt.Sprintf("fraud_%d_%s_%d.parquet", num, cType, cRatio)
 
-	fileName := fmt.Sprintf("fraud_%d_corrupted_%d.parquet", num, errorRatio)
 	//write
 	fw, err := local.NewLocalFileWriter(fileName)
 	if err != nil {
 		log.Println("Can't open file", err)
-		return
+		return err
 	}
 	pw, err := writer.NewParquetWriter(fw, new(Row), 10)
 	if err != nil {
 		log.Println("Can't create parquet writer", err)
-		return
+		return err
 	}
 
-	for i := 0; i < num; i++ {
-		row := NewRow(errorRatio)
+	for i := 0; uint(i) < num; i++ {
+		row := NewRow(cType, cRatio)
 		if err = pw.Write(row); err != nil {
 			log.Println("Write error", err)
 		}
@@ -133,4 +93,5 @@ func main() {
 	_ = fw.Close()
 
 	log.Printf("Corrupted %.0f/%d (%.2f%%)\n", corrupted, num, corrupted/float64(num)*100)
+	return nil
 }
